cd3ccd24f0726d6f5edc65914d236870cd54833b
Cameron Nelson <nelsonnoremac@gmail.com>
2022-04-05 16:45:58 -0500

    finished all tests

--- src/components/About/__tests__/index.text.js
+++ src/components/About/__tests__/index.text.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
-const { asFragment } = render(<About></About>);
+
 afterEach(cleanup)
 describe('About component', () => {
     it('renders', () => {
         render(<About></About>)
     });
     it('matches snapshot Dom node structure', () => {
+        const { asFragment } = render(<About></About>);
         expect(asFragment()).toMatchSnapshot();
     })
 })
